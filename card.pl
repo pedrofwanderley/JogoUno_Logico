ab2dff4a8bcf4e199fdaa79f5d519f35f362ab64
pedrofwanderley <pedro.wanderley@ccc.ufcg.edu.br>
2018-07-13 09:38:14 -0300

    representacao de carta simples criada, sem testes

--- card.pl
+++ card.pl
@@ -1,3 +1,23 @@
+colour(amarela).
+colour(verde).
+colour(azul).
+colour(vermelha).
+
+number( 2).
+number( 3).
+number( 4).
+number( 5).
+number( 6).
+number( 7).
+number( 8).
+number( 9).
+
+card(Colour,Number):-
+        colour(Colour), number(Number).
+
+
+
+
 check_card(amarela,  amarela).
 check_card(verde, verde).
 check_card(azul, azul).
