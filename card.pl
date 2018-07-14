9ebb3319a621400838eca8d8b0cdc9a35aafa3ae
pedrofwanderley <pedro.wanderley@ccc.ufcg.edu.br>
2018-07-14 00:05:44 -0300

    deck simples, sem cartas especiais

--- card.pl
+++ card.pl
@@ -14,6 +14,19 @@ number( 9).
 
 card(Colour,Number):-
         colour(Colour), number(Number).
+        
+deck([(2,amarela),(3,amarela),(4,amarela),
+      (5,amarela),(6,amarela),(7,amarela),(8,amarela),
+      (9,amarela),
+      (2,verde),(3,verde),(4,verde),
+      (5,verde),(6,verde),(7,verde),(8,verde),
+      (9,verde),
+      (2,azul),(3,azul),(4,azul),
+      (5,azul),(6,azul),(7,azul),(8,azul),
+      (9,azul),
+      (2,vermelha),(3,vermelha),(4,vermelha),
+      (5,vermelha),(6,vermelha),(7,vermelha),(8,vermelha),
+      (9,vermelha)]).
 
 
 
